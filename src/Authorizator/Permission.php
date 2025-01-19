9d87badae12b0dacf5c483a45732809a7c78609a
Václav Lukeš <vasek@flexsys.cz>
2025-01-19 19:49:19 +0100

    fixed Permission

--- src/Authorizator/Permission.php
+++ src/Authorizator/Permission.php
@@ -7,8 +7,16 @@ namespace Flexsyscz\Security\Authorizator;
 use Nette;
 
 
+/**
+ * @template T
+ */
 class Permission extends Nette\Security\Permission
 {
+	/**
+	 * @param string $appDir
+	 * @param string $tempDir
+	 * @return Permission<T>
+	 */
 	protected function loadResources(string $appDir, string $tempDir): self
 	{
 		$loader = new Nette\Loaders\RobotLoader();
@@ -18,10 +26,14 @@ class Permission extends Nette\Security\Permission
 
 		$loader->rebuild();
 
-		foreach ($loader->getIndexedClasses() as $className => $file) {
+		foreach (array_keys($loader->getIndexedClasses()) as $className) {
 			if (($interfaces = class_implements($className))) {
 				if (in_array(Nette\Security\Resource::class, $interfaces, true)) {
-					$this->addResource($className);
+					/** @var class-string $className */
+					$class = new \ReflectionClass($className);
+					if (!$class->isAbstract()) {
+						$this->addResource($className);
+					}
 				}
 			}
 		}
