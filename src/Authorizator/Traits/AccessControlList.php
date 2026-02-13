c24e842777fa3dab7b2cf7ad23a6eb8e4a915ce0
Václav Lukeš <vasek@flexsys.cz>
2026-02-13 10:23:14 +0100

    update check permissions for presenter signals

--- src/Authorizator/Traits/AccessControlList.php
+++ src/Authorizator/Traits/AccessControlList.php
@@ -7,12 +7,16 @@ namespace Flexsyscz\Security\Authorizator\Traits;
 use Flexsyscz\Security\Authorizator\Attributes\RequiredPermission;
 use Flexsyscz\Security\Authorizator\Permission;
 use Nette\InvalidStateException;
+use Nette\Utils\Strings;
 
 
 trait AccessControlList
 {
 	private Permission $acl;
 	private bool $authorized = false;
+
+	/** @var string[] */
+	private array $authorizedSignals = [];
 
 
 	public function injectAuthorizator(Permission $acl): void
@@ -26,6 +30,42 @@ trait AccessControlList
 		try {
 			$user = $this->getUser();
 			if ($user->isLoggedIn()) {
+				if ($this->getSignal() !== null) {
+					$authorizedSignal = false;
+					if ($this->getSignal()[0] !== '') {
+						$handler = sprintf('createComponent%s', Strings::firstUpper($this->getSignal()[0]));
+					} else {
+						$handler = sprintf('handle%s', Strings::firstUpper($this->getSignal()[1]));
+					}
+
+					if (!isset($this->authorizedSignals[$handler])) {
+						if (method_exists($this, $handler)) {
+							$ref = new \ReflectionClass($this);
+							$method = $ref->getMethod($handler);
+							$attributes = $method->getAttributes(RequiredPermission::class);
+							if (count($attributes) === 0) {
+								$authorizedSignal = true;
+							} else {
+								foreach ($attributes as $attribute) {
+									foreach ($attribute->getArguments() as $argument) {
+										if ($this->getUser()->isAllowed($this->getResourceId(), $argument)) {
+											$authorizedSignal = true;
+										}
+									}
+								}
+							}
+						} else {
+							$authorizedSignal = true;
+						}
+
+						$this->authorizedSignals[$handler] = $authorizedSignal;
+					}
+
+					if (!$this->authorizedSignals[$handler]) {
+						return false;
+					}
+				}
+
 				if (method_exists($element, 'getAttributes')) {
 					$attributes = $element->getAttributes(RequiredPermission::class);
 					foreach ($attributes as $attribute) {
